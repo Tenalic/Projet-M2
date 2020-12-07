0e851e7dc6dc9a2908bc46bc57c035a5d5c1de77
Tenalic <43466853+Tenalic@users.noreply.github.com>
2020-12-07 17:57:08 +0100

    Prize repo et service

---
+++ Back/src/main/java/projet/m2/back/repository/PrizeRepository.java
@@ -0,0 +1,13 @@
+package projet.m2.back.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import projet.m2.back.entity.Prize;
+
+import java.util.List;
+
+public interface PrizeRepository extends CrudRepository<Prize, Long> {
+
+    Prize findById(long id);
+
+}
