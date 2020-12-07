c5db0b06a269293614daf098a2672e21ea538a39
NekoRomain <romainwangon@gmail.com>
2020-12-07 11:18:18 +0100

    création strcuture projet Pas eclipse

--- projet_m2_back/src/main/java/projet_m2_back/Test.java
+++ projet_m2_back/src/main/java/projet_m2_back/Test.java
@@ -1,10 +1,10 @@
-package projet_m2_back;
+package main.java.projet_m2_back;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("test");
 	}
 
 }
