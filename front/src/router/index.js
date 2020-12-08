51196a86ec68ea5ba569893dbd40c832b911e262
AxelJanzegers <axeljanzegers@gmail.com>
2020-12-08 11:47:04 +0100

    up HomePage

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: Home,
+    meta: {
+      title: 'Bienvenue sur Monopolight :)'
+    }
+
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
   }
 ]
 
@@ -24,6 +26,11 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next(next)
 })
 
 export default router
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,94 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div align="center">
+  <v-carousel
+    cycle
+    height="250"
+    hide-delimiter-background
+    show-arrows-on-hover
+    interval="3000"
+  >
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet
+        :color="colors[i]"
+        height="100%"
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="display-3">
+            {{ slide }}
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+<v-container grid-list-md text-xs-center style="margin-top:3%">
+      <v-row align="center">
+        <v-flex xs6 offset-xs3 v-for="select in team" :key="select.MenuSelected" >
+          <v-hover v-slot:default="{ hover }">
+          <v-card
+          color="#d75838"
+          :elevation="hover ? 16 : 2"
+          class="menuTitle"
+          width="55%"
+          >
+            <v-card-title primary-title class="layout justify-center">
+            <v-car-text>
+              <div> {{select.MenuSelected}} </div>
+            </v-car-text>
+            </v-card-title>
+          </v-card>
+          </v-hover>
+        </v-flex>
+      </v-row>
+    </v-container>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      team: [
+        { MenuSelected: 'allo' },
+        { MenuSelected: 'allo' },
+        { MenuSelected: 'allo' },
+        { MenuSelected: 'allo' },
+        { MenuSelected: 'allo' }
+      ],
+      colors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4'
+      ],
+      slides: [
+        'First',
+        'Second',
+        'Third',
+        'Fourth',
+        'Fifth'
+      ]
+    }
   }
 }
 </script>
+
+<style scoped>
+.menuTitle {
+  margin: 3%;
+  cursor: pointer;
+}
+
+.v-card {
+  color: whitesmoke !important;
+}
+
+</style>
