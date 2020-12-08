85efcd3773136d2f6e1242e662b9b7ddb12918a3
Arthur <arthurquique@protonmail.com>
2020-12-08 16:13:19 +0100

    correction visuel

--- front/src/plugins/vuetify.js
+++ front/src/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.use(Vuetify)
 
@@ -7,8 +8,8 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: '#776dbb',
-        secondary: '#ffffff'
+        primary: '#d75838',
+        secondary: '#eb6746'
       }
     }
   }
