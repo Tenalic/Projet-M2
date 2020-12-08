840edc4d263a908401aeebf91fe857219aadb369
Delphin <delphinrukundo10@gmail.com>
2020-12-08 10:41:11 +0100

    replace  v-layout with v-col

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,19 +1,16 @@
 <template>
   <v-footer id="footer" padless>
-    <v-row no-gutters>
-      <v-col>
-        <v-layout class="primary py-3" justify-center wrap>
-          <v-btn v-for="(item, index) in footerLinks" :key="index" :to="item.link" color="white" large text tile>{{ item.title }}</v-btn>
-        </v-layout>
-      <v-divider class="primary darken-3"></v-divider>
-      <v-layout class="primary py-2" justify-center wrap>
+    <v-row no-gutters class="grey darken-4">
+      <v-col cols="12" class="primary text-center py-2">
+            <v-btn v-for="(item, index) in footerLinks" :key="index" :to="item.link" color="white" large text tile>{{ item.title }}</v-btn>
+      </v-col>
+      <v-col cols="12" class="primary text-center py-2 mt-1">
         <v-btn v-for="(social, index) in socialLinks" :key="index" href="#" dark class="mx-3" icon>
           <v-icon size="24px">{{ social.icon }}</v-icon>
         </v-btn>
-      </v-layout>
-      <v-layout justify-center grey darken-4 py-2 white--text>
-        <p> &copy; {{ new Date().getFullYear() }} — <strong>Monopolight.com</strong> </p>
-      </v-layout>
+      </v-col>
+      <v-col cols="12" class="grey darken-4 text-center white--text pt-3">
+        <p color="white"> &copy; {{ new Date().getFullYear() }} — <strong>Monopolight.com</strong> </p>
       </v-col>
     </v-row>
   </v-footer>
