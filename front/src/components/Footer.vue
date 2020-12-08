775cbf619ccd3e4abd14994a1f5131ba3e3ee47e
Delphin <delphinrukundo10@gmail.com>
2020-12-08 12:49:21 +0100

    fix the footer

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -1,10 +1,10 @@
 <template>
   <v-footer id="footer" padless>
-    <v-row no-gutters class="grey darken-4">
+    <v-row no-gutters>
       <v-col cols="12" class="primary text-center py-2">
             <v-btn v-for="(item, index) in footerLinks" :key="index" :to="item.link" color="white" large text tile>{{ item.title }}</v-btn>
       </v-col>
-      <v-col cols="12" class="primary text-center py-2 mt-1">
+      <v-col cols="12" class="primary text-center py-2 social-links">
         <v-btn v-for="(social, index) in socialLinks" :key="index" href="#" dark class="mx-3" icon>
           <v-icon size="24px">{{ social.icon }}</v-icon>
         </v-btn>
@@ -24,8 +24,8 @@ export default {
         { title: 'Home', link: '/' },
         { title: 'Contact us', link: '/contact' },
         { title: 'About us', link: '/about' },
-        { icon: 'info', title: 'Sign in', link: '/sign-in' },
-        { icon: 'info', title: 'Sign up', link: '/sign-up' }
+        { title: 'Sign in', link: '/sign-in' },
+        { title: 'Sign up', link: '/sign-up' }
       ],
       socialLinks: [
         { icon: 'mdi-facebook', link: 'https://www.facebook.com/' },
@@ -42,5 +42,9 @@ export default {
 <style lang="css" scoped>
   #footer {
     bottom: 0;
+  }
+
+  .social-links {
+    margin-top: 2px;
   }
 </style>
