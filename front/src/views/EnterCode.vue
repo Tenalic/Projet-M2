bc6f162189d83f02d8fd20164a629a74b016d452
EmmZak <emmanuelzakaryan@gmail.com>
2020-12-09 14:26:05 +0100

    enter code input done

--- front/src/views/EnterCode.vue
+++ front/src/views/EnterCode.vue
@@ -1,7 +1,31 @@
 <template>
-<div align="center" justify="center">
-  enter code page
-  </div>
+  <v-container fill-height>
+    <v-row align="center" justify="center">
+      <v-col md="6">
+        <v-card outlined>
+          <v-card-title>
+            Saisir votre code
+          </v-card-title>
+          <v-card-text>
+            <v-form ref="loginForm" v-model="valid">
+              <v-container>
+                <v-row justify="start">
+                  <v-col md="10">
+                    <v-text-field clearable dense outlined required v-model="code" :rules="emailRules" label="Votre code"></v-text-field>
+                  </v-col>
+                </v-row>
+                <v-row justify="start">
+                  <v-col>
+                    <v-btn :disabled="!valid" color="success" class="mr-2" @click="submit">Valider</v-btn>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 // @ is an alias to /src
@@ -10,39 +34,8 @@ export default {
   name: 'EnterCode',
   data () {
     return {
-      team: [
-        { title: 'Cadeaux', link: '/cadeaux' },
-        { title: 'allo' },
-        { title: 'allo' },
-        { title: 'allo' },
-        { title: 'Profile', link: '/profile' }
-      ],
-      colors: [
-        'indigo',
-        'warning',
-        'pink darken-2',
-        'red lighten-1',
-        'deep-purple accent-4'
-      ],
-      slides: [
-        'First',
-        'Second',
-        'Third',
-        'Fourth',
-        'Fifth'
-      ]
+      code: ''
     }
   }
 }
 </script>
-
-<style scoped>
-.menuTitle {
-  cursor: pointer;
-}
-
-.v-card {
-  color: whitesmoke !important;
-}
-
-</style>
