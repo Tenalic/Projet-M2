b8413bf3e080ab1f2e185f289c8884f058f9805a
Tenalic <43466853+Tenalic@users.noreply.github.com>
2020-12-09 11:18:05 +0100

    useCode fini (sans le gain de prize)

--- Back/src/main/java/projet/m2/back/controller/CodeController.java
+++ Back/src/main/java/projet/m2/back/controller/CodeController.java
@@ -1,7 +1,10 @@
 package projet.m2.back.controller;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import projet.m2.back.entity.Account;
 import projet.m2.back.service.interfaces.ICodeService;
 
 @RestController
@@ -11,8 +14,33 @@ public class CodeController {
     private ICodeService codeService;
 
     @PostMapping("/code/use/{code}")
-    public int useCode(@RequestHeader(value = "IdAccount") long idAccount, @PathVariable String code) {
-        return codeService.useCode(idAccount, code);
+    public ResponseEntity useCode(@RequestHeader(value = "IdAccount") long idAccount, @PathVariable String code) {
+
+        Object response = codeService.useCode(idAccount, code);
+
+        JSONObject responseJSON = new JSONObject();
+
+        if (response instanceof Integer) {
+            Integer codeErreur = (Integer) response;
+            switch (codeErreur) {
+                case 1:
+                    responseJSON.put("message", "Error: Code non trouvé");
+                    break;
+                case 2:
+                    responseJSON.put("message", "Error: Code déjà utilisé");
+                    break;
+                case 3:
+                    responseJSON.put("message", "Error: Code inconue");
+                    break;
+                default:
+                    responseJSON.put("message", "Error: Erreur inconue");
+                    break;
+            }
+            return ResponseEntity.status(200).body(responseJSON);
+        } else {
+            Account account = (Account) response;
+            return ResponseEntity.status(200).body(account);
+        }
     }
 
 
--- Back/src/main/java/projet/m2/back/service/interfaces/ICodeService.java
+++ Back/src/main/java/projet/m2/back/service/interfaces/ICodeService.java
@@ -5,9 +5,9 @@ public interface ICodeService {
     /**
      * @param idAccount : id du compte ou l'on souhaite appliqué le code
      * @param code      : code du code
-     * @return 0 : ok, 1 : compte non trouvé, 2 : code deja utilisé
+     * @return 0 : ok, 1 : compte non trouvé, 2 : code deja utilisé, 3 : code inconue
      */
-    int useCode(final long idAccount, final String code);
+    Object useCode(final long idAccount, final String code);
 
     int changeUsed(final long id, final boolean used);
 
