77b27a12ed5649a2bcee27249604697f469260ea
Arthur <arthurquique@protonmail.com>
2020-12-09 17:40:40 +0100

    [Feat] Affichage Plateau API

--- front/src/views/GameView.vue
+++ front/src/views/GameView.vue
@@ -1,73 +1,76 @@
 <template>
-  <v-container>
-    <v-row>
-      <!-- PLATEAU -->
-      <v-sheet
-      color="white"
-      elevation="20"
-      height="600"
-      width="600">
-          <v-row
-          v-for="i in 6"
-          :key="i"
-          no-gutters>
-            <v-col
-            v-for="j in 6"
-            :key="j"
+  <v-container v-if="!loading">
+      <v-row justify="center">
+        <!-- PLATEAU -->
+        <v-sheet
+        v-if="board"
+        color="white"
+        elevation="20"
+        height="600"
+        width="600">
+            <v-row
+            v-for="i in 6"
+            :key="i"
             no-gutters>
-                <!-- Si le contenu de displayBoard à l'index i,j n'est pas nul,
-                      on affiche les coordonnées de la rue -->
-                <v-card v-if="displayBoard[i-1][j-1] != null"
-                tile
-                outline
-                v-bind:style="{'background-color' : displayBoard[i-1][j-1].color}"
-                v-bind:id = displayBoard[i-1][j-1].id
-                height="100"
-                width="100">
-                  {{displayBoard[i-1][j-1].id}}  <!-- id pour se repérer, à enlever plus tard -->
-                  {{displayBoard[i-1][j-1].streetName}}<br>
-                  {{displayBoard[i-1][j-1].cost}}€
-                </v-card>
-            </v-col>
-          </v-row>
-      </v-sheet>
-      <!-- INFOS JOUEURS -->
+              <v-col
+              v-for="j in 6"
+              :key="j"
+              no-gutters>
+                  <!-- Si le contenu de displayBoard à l'index i,j n'est pas nul,
+                        on affiche les coordonnées de la rue -->
+                  <v-card v-if="displayBoard[i-1][j-1] != null"
+                  tile
+                  outline
+                  v-bind:style="{'background-color' : displayBoard[i-1][j-1].color}"
+                  v-bind:id = displayBoard[i-1][j-1].index
+                  height="100"
+                  width="100">
+                    {{displayBoard[i-1][j-1].index}}  <!-- id pour se repérer, à enlever plus tard -->
+                    {{displayBoard[i-1][j-1].streetName}}<br>
+                    {{displayBoard[i-1][j-1].cost}}€
+                  </v-card>
+              </v-col>
+            </v-row>
+        </v-sheet>
+        <!-- INFOS JOUEURS -->
+        <v-card>
+          <v-card-title>
+            {{ account.nickname }}
+          </v-card-title>
+        <v-card-subtitle>
+          Argent : {{account.credit}}€
+        </v-card-subtitle>
+        <v-divider></v-divider>
+        <v-treeview></v-treeview>
+        </v-card>
+      </v-row>
+      <!-- BOUTON LANCER DE DES -->
+      <v-btn id="diceToss" @click="rollTheDices">
+        <v-icon small>fas fa-dice-five</v-icon>{{account.nbDice}}
+        Lancez vos dés
+      </v-btn>
+      <!-- BOUTON ENTRER UN CODE -->
+      <v-btn>
+        Entrer un code
+      </v-btn>
+      <!-- CARTE D'INFOS SUR LA RUE -->
       <v-card>
-        <v-card-title>
-          {{ account.nickname }}
-        </v-card-title>
+      <v-progress-linear value="100" :color="board[piece.index].color"/>
+      <v-card-title>
+        {{ board[piece.index].index }} {{ board[piece.index].streetName }}
+      </v-card-title>
       <v-card-subtitle>
-        Argent : {{account.credit}}€
+        Valeur : {{ board[piece.index].cost }}€
       </v-card-subtitle>
-      <v-divider></v-divider>
-      <v-treeview></v-treeview>
+      <v-btn :id="buyButton" @click="buy(piece.index)" :disabled="!canBuy">Acheter</v-btn>
+      <v-btn>Refuser</v-btn>
       </v-card>
-    </v-row>
-    <!-- BOUTON LANCER DE DES -->
-    <v-btn id="diceToss" @click="rollTheDices">
-      <v-icon small>fas fa-dice-five</v-icon>{{account.nbDice}}
-      Lancez vos dés
-    </v-btn>
-    <!-- BOUTON ENTRER UN CODE -->
-    <v-btn>
-      Entrer un code
-    </v-btn>
-    <!-- CARTE D'INFOS SUR LA RUE -->
-    <v-card>
-    <v-progress-linear value="100" :color="board[piece.index].color"/>
-    <v-card-title>
-      {{ board[piece.index].id }} {{ board[piece.index].streetName }}
-    </v-card-title>
-    <v-card-subtitle>
-      Valeur : {{ board[piece.index].cost }}€
-    </v-card-subtitle>
-    <v-btn :id="buyButton" @click="buy(piece.index)" :disabled="!canBuy">Acheter</v-btn>
-    <v-btn>Refuser</v-btn>
-    </v-card>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'GameView',
   data () {
@@ -78,148 +81,7 @@ export default {
         credit: 50,
         nbDice: 2
       },
-      board: [
-        {
-          id: 0,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'white',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 1,
-          streetName: 'Rue de la Paix',
-          color: 'red',
-          cost: 100,
-          bought: false
-        },
-        {
-          id: 2,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 3,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'green',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 4,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 5,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 6,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 7,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 8,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 9,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 10,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 11,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 12,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 13,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 14,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 15,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 16,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 17,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 18,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        },
-        {
-          id: 19,
-          streetName: 'Avenue des Champs-Elysées',
-          color: 'blue',
-          cost: 50,
-          bought: false
-        }
-      ],
+      board: null,
       // Tableau pour l'affichage du plateau
       displayBoard: [
         [null, null, null, null, null, null],
@@ -235,42 +97,60 @@ export default {
       piece: {
         icon: 'fas fa-chess-pawn',
         index: -1
-      }
-
+      },
+      loading: true
     }
   },
-  beforeMount () {
-    this.setDiplayBoard()
+  mounted () {
+    this.getBoard()
     this.piece.index = 0
   },
   methods: {
+    /*
+    * Création et affichage du plateau
+    */
+    // Réception du plateau depuis l'API
+    async getBoard () {
+      await axios
+        .get('http://monopolym2tnsi.hopto.org:8080/board')
+        .then(response => { this.board = response.data.board; this.setDiplayBoard() })
+        .catch(error => console.log(error))
+    },
     // Création du tableau displayBoard
     setDiplayBoard () {
       let i = 0
       let n = 5
       let m = 5
       for (m = 5; m >= 0; m--) {
+        console.log(i + ' ' + this.board[i])
         this.displayBoard[n][m] = this.board[i]
         i += 1
+        console.log(this.board[i])
       }
       m = 0
       for (n = 4; n >= 0; n--) {
+        console.log(i + ' ' + this.board[i])
         this.displayBoard[n][m] = this.board[i]
         i += 1
       }
       n = 0
       for (m = 1; m < 6; m++) {
+        console.log(i + ' ' + this.board[i])
         this.displayBoard[n][m] = this.board[i]
         i += 1
       }
       m = 5
       for (n = 1; n < 5; n++) {
+        console.log(i + ' ' + this.board[i])
         this.displayBoard[n][m] = this.board[i]
         i += 1
       }
+      console.log(this.displayBoard)
+      this.loading = false
     },
     // Lance les 2 dés
-    rollTheDices () {
+    async rollTheDices () {
+      // APPEL API
       this.diceToss = Math.floor(Math.random() * (12 - 2 + 1) + 2)
       this.advance()
     },
