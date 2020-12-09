b710428327a4609163a07e36f5fb5e4ca8596fee
willLarcy <Larcywilliam@gmail.com>
2020-12-09 13:13:23 +0100

    ajout fonction prizeRandom dans PrizeServiceImpl

--- Back/src/main/java/projet/m2/back/service/implementations/PrizeServiceImpl.java
+++ Back/src/main/java/projet/m2/back/service/implementations/PrizeServiceImpl.java
@@ -2,12 +2,16 @@ package projet.m2.back.service.implementations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import projet.m2.back.entity.Account;
 import projet.m2.back.entity.Prize;
 import projet.m2.back.repository.PrizeRepository;
 import projet.m2.back.service.interfaces.IPrizeService;
 import projet.m2.back.service.interfaces.IUtils;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
 
 @Service
 public class PrizeServiceImpl implements IPrizeService {
@@ -82,4 +86,56 @@ public class PrizeServiceImpl implements IPrizeService {
         p = new Prize("prize8",4 );
         repo.save(p);
     }
+
+
+    public void randomPrize (int choice, Account account)  //choice comporte le resultat de gain
+    {
+        if(choice == 0)//gagne juste un dé
+        {
+            account.setNbDice(account.getNbDice() + 1);
+        }
+        if (choice == 1) //gagne un dé + 50pts
+        {
+            account.setNbDice(account.getNbDice() + 1);
+            account.setCredit(account.getCredit() + 50);
+        }
+        if (choice == 2) // gagne un dé + 100 pts
+        {
+            account.setNbDice(account.getNbDice() + 1);
+            account.setCredit(account.getCredit() + 100);
+        }
+        if (choice == 3)  // gagne un dé + 150pts
+        {
+            account.setNbDice(account.getNbDice() + 1);
+            account.setCredit(account.getCredit() + 150);
+        }
+        if (choice == 4)  //donne un dé et un prix aléatoire au compte
+        {
+            Random r = new Random();
+            account.setNbDice(account.getNbDice() + 1);
+            Iterable<Prize> tmp = getAllPrize();
+            ArrayList<Prize> listPrize = new ArrayList<>();
+            for(Prize p : tmp)
+            {
+                listPrize.add(p);
+            }
+            int tmpRand = r.nextInt(listPrize.size());
+            Collection<Prize> prize = account.getPrize();
+            prize.add(listPrize.get(tmpRand));
+            //delete le prix de la bdd si il y en a qu'un
+            if(listPrize.get(tmpRand).getQuantity() == 1)
+            {
+                repo.delete(listPrize.get(tmpRand));
+            }
+            else
+            {
+                //décrementer la quantité
+                listPrize.get(tmpRand).setQuantity(listPrize.get(tmpRand).getQuantity() - 1);
+                //update le prize dans la bdd
+
+            }
+
+        }
+    }
+
 }
