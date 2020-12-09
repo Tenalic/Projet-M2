57f04d8aec1745290ec80ffbfd31567c100b44db
EmmZak <emmanuelzakaryan@gmail.com>
2020-12-09 17:35:40 +0100

    added rules for coupon

--- front/src/views/EnterCode.vue
+++ front/src/views/EnterCode.vue
@@ -1,22 +1,23 @@
 <template>
   <v-container fill-height>
-    <v-row align="center" justify="center">
-      <v-col md="6">
+    <v-row align='center' justify='center'>
+      <v-col md='6'>
         <v-card outlined>
           <v-card-title>
             Saisir votre code
           </v-card-title>
           <v-card-text>
-            <v-form ref="loginForm" v-model="valid">
+            <v-form ref='code' v-model='valid'>
               <v-container>
-                <v-row justify="start">
-                  <v-col md="10">
-                    <v-text-field clearable dense outlined required v-model="code" :rules="emailRules" label="Votre code"></v-text-field>
+                <v-row justify='start'>
+                  <v-col md='10'>
+                    <v-text-field clearable dense outlined required v-model='code' :rules='codeRules' label='Votre coupon'></v-text-field>
+                    <h1 v-if="couponNonValide"> Coupon non valide </h1>
                   </v-col>
                 </v-row>
-                <v-row justify="start">
+                <v-row justify='start'>
                   <v-col>
-                    <v-btn :disabled="!valid" color="success" class="mr-2" @click="submit">Valider</v-btn>
+                    <v-btn :disabled='!valid' color='success' class='mr-2' @click='get_prizes'>Valider</v-btn>
                   </v-col>
                 </v-row>
               </v-container>
@@ -35,12 +36,49 @@ export default {
   name: 'EnterCode',
   data () {
     return {
-      code: ''
+      label: 'Votre coupon',
+      couponNonValide: false,
+      validCod: true,
+      valid: true,
+      code: '',
+      codeRules: [
+        v => /^[0-9]/.test(v) || 'Seuls les chiffres sont acceptés'
+      ],
+      prize1: {
+        nbDice: 988
+      },
+      prize2: {
+        nbDice: 989,
+        creditWin: 50,
+        credit: 9750
+      },
+      prize3: {
+        nbDice: 991,
+        prizeWin: 'prize2',
+        prize: [
+          'prize4',
+          'prize1',
+          'prize7'
+        ]
+      },
+      prizeError: {
+        message: 'Error: ça va pas le faire'
+      }
     }
   },
   methods: {
-    submit () {
-      axios.post()
+    get_prizes () {
+      var obj = axios.get()
+      obj = this.prizeError
+      if ('message' in obj) {
+        console.log('error ', obj.message)
+        console.log('code ', this.code)
+        if (this.code === '123') {
+          console.log('errur validé 123')
+          console.log('after')
+          this.couponNonValide = true
+        }
+      }
     }
   }
 }
