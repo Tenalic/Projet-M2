a9698bd05ee8db5d542cac9c23cfac604de574f6
Delphin <delphinrukundo10@gmail.com>
2020-12-09 14:34:37 +0100

    update about us page

--- front/src/views/About.vue
+++ front/src/views/About.vue
@@ -1,16 +1,17 @@
 <template>
   <v-container fluid mt-5>
-    <v-row justify="center" align="center">
-      <img src="../assets/logotnsi.png" width="400" @click="goToHome" />
+    <v-row justify="center" align="center" no-gutters>
+      <img src="../assets/logotnsi.png" height="300" width="auto" @click="goToHome" />
     </v-row>
-    <v-row justify="center" align="center">
+    <v-row justify="center" align="center" no-gutters>
       <v-col md="6">
         <v-card flat color="mainBackground transparent">
           <p style="font-size: 24px; color: #b52225">
             Monopolight est un projet réalisé par les étudiants de seconde année
             de Master TNSI FI à l'Université Polytechnique des Hauts-de-France. Entrez vos
-            codes sur ce site web, jouez, et remportez des prix ! Egalement disponible sur
-            mobile ! Amusez vous bien !
+            codes sur ce site web, jouez, et remportez des prix ! <br><br>
+            Egalement disponible sur mobile ! <br><br>
+            Amusez vous bien !
           </p>
         </v-card>
       </v-col>
