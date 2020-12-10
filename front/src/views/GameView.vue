ea5b6e597360e9ce5877609444284446a1558e58
Arthur <arthurquique@protonmail.com>
2020-12-10 10:39:31 +0100

    [Feat] Ajout de l'affichage des dés+appel api dice

--- front/src/views/GameView.vue
+++ front/src/views/GameView.vue
@@ -20,7 +20,7 @@
                         on affiche les coordonnées de la rue -->
                   <v-card v-if="displayBoard[i-1][j-1] != null"
                   tile
-                  outline
+                  outlined
                   v-bind:style="{'background-color' : displayBoard[i-1][j-1].color}"
                   v-bind:id = displayBoard[i-1][j-1].index
                   height="100"
@@ -42,27 +42,31 @@
         </v-card-subtitle>
         <v-divider></v-divider>
         <v-treeview></v-treeview>
+        <!-- BOUTON LANCER DE DES -->
+        <v-btn id="diceToss" @click="rollTheDices">
+          <v-icon small>fas fa-dice-five</v-icon>{{account.nbDice}}
+          Lancez vos dés
+        </v-btn>
+        <!-- BOUTON ENTRER UN CODE -->
+        <v-btn>
+          Entrer un code
+        </v-btn>
+        <v-card v-if="diceToss" id="dice">
+          <img v-bind:src="diceImage1" width="60">
+          <img v-bind:src="diceImage2" width="60">
+        </v-card>
         </v-card>
       </v-row>
-      <!-- BOUTON LANCER DE DES -->
-      <v-btn id="diceToss" @click="rollTheDices">
-        <v-icon small>fas fa-dice-five</v-icon>{{account.nbDice}}
-        Lancez vos dés
-      </v-btn>
-      <!-- BOUTON ENTRER UN CODE -->
-      <v-btn>
-        Entrer un code
-      </v-btn>
       <!-- CARTE D'INFOS SUR LA RUE -->
       <v-card>
-      <v-progress-linear value="100" :color="board[piece.index].color"/>
+      <v-progress-linear value="100" :color="board[account.indexSquare].color"/>
       <v-card-title>
-        {{ board[piece.index].index }} {{ board[piece.index].streetName }}
+        {{ board[account.indexSquare].index }} {{ board[account.indexSquare].streetName }}
       </v-card-title>
       <v-card-subtitle>
-        Valeur : {{ board[piece.index].cost }}€
+        Valeur : {{ board[account.indexSquare].cost }}€
       </v-card-subtitle>
-      <v-btn :id="buyButton" @click="buy(piece.index)" :disabled="!canBuy">Acheter</v-btn>
+      <v-btn :id="buyButton" @click="buy(account.indexSquare)" :disabled="!canBuy">Acheter</v-btn>
       <v-btn>Refuser</v-btn>
       </v-card>
   </v-container>
@@ -77,9 +81,11 @@ export default {
     return {
       // Utilisateur
       account: {
+        id: 310,
         nickname: 'Nono',
         credit: 50,
-        nbDice: 2
+        nbDice: 2,
+        indexSquare: 0
       },
       board: null,
       // Tableau pour l'affichage du plateau
@@ -92,7 +98,7 @@ export default {
         [null, null, null, null, null, null]
       ],
       // Résultat du lancé de dés
-      diceToss: 0,
+      diceToss: null,
       // Pion
       piece: {
         icon: 'fas fa-chess-pawn',
@@ -110,8 +116,8 @@ export default {
     * Création et affichage du plateau
     */
     // Réception du plateau depuis l'API
-    async getBoard () {
-      await axios
+    getBoard () {
+      axios
         .get('http://monopolym2tnsi.hopto.org:8080/board')
         .then(response => { this.board = response.data.board; this.setDiplayBoard() })
         .catch(error => console.log(error))
@@ -150,9 +156,18 @@ export default {
     },
     // Lance les 2 dés
     async rollTheDices () {
-      // APPEL API
-      this.diceToss = Math.floor(Math.random() * (12 - 2 + 1) + 2)
-      this.advance()
+      await axios
+        .post('http://monopolym2tnsi.hopto.org:8080/account/dice',
+          {},
+          { headers: { IdAccount: this.account.id } })
+        .then(response => {
+          console.log(response)
+          this.account.nbDice = response.data.nbDice
+          this.account.indexSquare = response.data.indexSquare
+          this.diceToss = response.data.diceResult
+          console.log(this.diceToss)
+        })
+        .catch(error => console.log(error))
     },
     // Faire avancer le pion
     advance () {
@@ -171,6 +186,17 @@ export default {
     // Fonction achat
     buy (index) {
       this.account.credit -= this.board[index].cost
+    },
+    getDiceImage (n) {
+      switch (n) {
+        case 1: return require('../assets/dices/dice1.png')
+        case 2: return require('../assets/dices/dice2.png')
+        case 3: return require('../assets/dices/dice3.png')
+        case 4: return require('../assets/dices/dice4.png')
+        case 5: return require('../assets/dices/dice5.png')
+        case 6: return require('../assets/dices/dice6.png')
+        default: return null
+      }
     }
   },
   watch: {
@@ -187,6 +213,12 @@ export default {
     // Retourne si le joueur a assez d'argent pour payer
     canBuy () {
       return (this.account.credit >= this.board[this.piece.index].cost)
+    },
+    diceImage1 () {
+      return this.getDiceImage(this.diceToss[0])
+    },
+    diceImage2 () {
+      return this.getDiceImage(this.diceToss[1])
     }
   }
 }
