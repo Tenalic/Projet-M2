05976448c28486cc9abaa48fefc47350bc157e21
Tenalic <43466853+Tenalic@users.noreply.github.com>
2020-12-10 10:09:06 +0100

    JavaDoc CodeService

--- Back/src/main/java/projet/m2/back/service/interfaces/ICodeService.java
+++ Back/src/main/java/projet/m2/back/service/interfaces/ICodeService.java
@@ -3,13 +3,25 @@ package projet.m2.back.service.interfaces;
 public interface ICodeService {
 
     /**
+     * Utilise un code et ajoute les gains au compte
+     *
      * @param idAccount : id du compte ou l'on souhaite appliqué le code
      * @param code      : code du code
      * @return 0 : ok, 1 : compte non trouvé, 2 : code deja utilisé, 3 : code inconue
      */
     Object useCode(final long idAccount, final String code);
 
+    /**
+     * Change si le code est utilisé ou non en base
+     *
+     * @param id   : id du code
+     * @param used : si il est utilisé ou non
+     * @return 0
+     */
     int changeUsed(final long id, final boolean used);
 
+    /**
+     * Ajoute en base 200 codes générés aléatoirement
+     */
     void createDatasetCode();
 }
