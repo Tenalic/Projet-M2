04a667b918e32d4012b82ca481ba967412a47c33
EmmZak <emmanuelzakaryan@gmail.com>
2020-12-10 10:58:10 +0100

    merging

--- front/src/views/EnterCode.vue
+++ front/src/views/EnterCode.vue
@@ -21,6 +21,64 @@
                   </v-col>
                 </v-row>
               </v-container>
+              <!-- list prizes container -->
+              <!-- just won in GREEN -->
+              <v-container v-if="finalPrize" fluid mt-5>
+                <v-card>
+                  <v-row align="center" justify="center">
+                    <v-col md="6">
+                      <v-card-title>
+                        You won {{ finalPrize.prizeWin }}
+                      </v-card-title>
+                    </v-col>
+                  </v-row>
+                </v-card>
+              </v-container>
+
+              <v-container v-if="isListPrize" fluid mt-5>
+                <v-row align="center" justify="center" v-for="p in this.prize3.prize" :key="p.MenuSelected">
+                  <v-col md="6">
+                    <v-hover v-slot:default="{ hover }">
+                    <v-card
+                    color="#d75838"
+                    :elevation="hover ? 16 : 2"
+                    class="menuTitle"
+                    width="60%"
+                    >
+                      <v-card-title primary-title class="layout justify-center">
+                      <!-- <v-card-text> -->
+                        <div> {{p}} </div>
+                      <!-- </v-card-text> -->
+                      </v-card-title>
+                    </v-card>
+                    </v-hover>
+                  </v-col>
+                </v-row>
+              </v-container>
+              <!-- list prizes container -->
+
+              <!-- simple prize container -->
+              <v-container v-if="isSimplePrize" fluid mt-5>
+                <v-row align="center" justify="center">
+                  <v-col md="6">
+                    <v-card>
+                      <v-card-title>simple</v-card-title>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-container>
+              <!-- simple prize container -->
+              <!-- credit prize container -->
+              <v-container v-if="isCreditPrize" fluid mt-5>
+                <v-row align="center" justify="center">
+                  <v-col md="6">
+                    <v-card>
+                      <v-card-title>credit</v-card-title>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-container>
+              <!-- credit prize container -->
             </v-form>
           </v-card-text>
         </v-card>
@@ -64,37 +122,58 @@ export default {
       prizeError: {
         message: 'Error: ça va pas le faire'
       },
-      requestRes: null
+      requestRes: null,
+      // received object
+      finalPrize: null,
+      isSimplePrize: true,
+      isCreditPrize: true,
+      isListPrize: true
     }
   },
   methods: {
+    render () {
+
+    },
+    render_credit () {
+
+    },
+    render_prizes () {
+
+    },
+    render_error () {
+
+    },
     get_prizes () {
       console.log('get_prizes of ', this.code)
       // var obj = null
       // test code 9559948 or 4430164 or 3030553
+
       const config = {
         headers: {
-          IdAccount: 123
+          IdAccount: 180
         }
       }
+      console.log('confog ', config)
       const data = {
       }
-
       // var obj = null
-      // var headers = { IdAccount: 123 }
+      // var headers = { IdAccount: 310 }
       var url = 'http://monopolym2tnsi.hopto.org:8080/code/use/' + this.code
       axios.post(url, data, config)
         .then(res => {
-          // console.log(res.data)
+          console.log('post.then ', res.data)
           var obj = res.data
           if ('message' in obj) {
-            console.log('Error: ', obj.message)
+            this.render_error()
           }
           if ('credit' in obj) {
+            this.render_credit()
           }
           if ('prize' in obj) {
+            this.render_prizes()
           }
           if (!('credit' in obj) && !('prize' in obj) && !('message' in obj)) {
+            this.render()
           }
         })
         .catch(err => console.log('error ', err))
@@ -102,3 +181,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+.menuTitle {
+  cursor: pointer;
+}
+</style>
