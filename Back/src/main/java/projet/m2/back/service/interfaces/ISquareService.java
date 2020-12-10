fe3930f817a27978ba67ce87701ec2b93ba89a1a
Tenalic <43466853+Tenalic@users.noreply.github.com>
2020-12-10 09:58:58 +0100

    JavaDoc SquareService

--- Back/src/main/java/projet/m2/back/service/interfaces/ISquareService.java
+++ Back/src/main/java/projet/m2/back/service/interfaces/ISquareService.java
@@ -3,7 +3,28 @@ package projet.m2.back.service.interfaces;
 import projet.m2.back.entity.Square;
 
 public interface ISquareService {
+
+    /**
+     * Récupérer le cout d'une case en fonction de son index
+     *
+     * @param indexSquare : l'index de la case
+     * @return le cout de l'index
+     */
     int getCost(int indexSquare);
+
+    /**
+     * Récupérer une case par son index
+     *
+     * @param indexSquare : l'index de la case
+     * @return la case, null sinon
+     */
     Square getSquare(int indexSquare);
+
+    /**
+     * Créer une case en base de donnée
+     *
+     * @param square : la case a crée
+     * @return La case crée si ok, null sinon
+     */
     Square createSquare(Square square);
 }
