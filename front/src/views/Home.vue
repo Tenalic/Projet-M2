de7944dbcaad35bc474837ddf295dc05055a57fa
AxelJanzegers <axeljanzegers@gmail.com>
2020-12-11 12:43:42 +0100

    up visu Home (banner + add v-card)

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <div align="center">
-  <v-carousel cycle height="175" hide-delimiter-background show-arrows-on-hover interval="3000">
+  <v-carousel cycle height="165" hide-delimiter-background show-arrows-on-hover interval="3000">
     <v-carousel-item v-for="(slide, i) in slides" :key="i">
       <v-sheet :color="colors[i]" height="100%">
         <v-row class="fill-height" align="center" justify="center">
@@ -15,7 +15,7 @@
       <v-row align="center" justify="center" v-for="select in team" :key="select.link" class="mt-10">
         <v-col md="6">
           <v-hover v-slot:default="{ hover }">
-          <v-card color="#d75838" :elevation="hover ? 16 : 2" class="menuTitle" width="45%" :to="select.link">
+          <v-card color="#d75838" :elevation="hover ? 16 : 2" class="menuTitle white--text" width="45%" :to="select.link">
             <v-card-title primary-title class="layout justify-center">
             <!-- <v-card-text> -->
               <div> {{select.title}} </div>
@@ -23,6 +23,39 @@
             </v-card-title>
           </v-card>
           </v-hover>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col md="6">
+          <v-card
+          width="340"
+          height="340">
+          <v-img
+          src="../assets/tnsi.png"
+          height="200"
+          >
+          </v-img>
+          <v-car-title> Restaurant Tnsi </v-car-title>
+          <v-card-subtitle>
+          Bientôt de nouveaux restaurants proche de chez vous :)
+          </v-card-subtitle>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+          width="340"
+          height="340">
+          <v-img
+          src="../assets/security.png"
+          height="200"
+          >
+          </v-img>
+          <v-car-title> La sécurité de tous </v-car-title>
+          <v-card-subtitle>
+          Port du masque obligatoire, gel hydroalcoolique dans tous nos restaurants, respect de la distanciation sociale.
+          Faites attention à vous, prenez soin de votre entourage :)
+          </v-card-subtitle>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -43,14 +76,16 @@ export default {
         'warning',
         'pink darken-2',
         'red lighten-1',
-        'deep-purple accent-4'
+        'deep-purple accent-4',
+        'black'
       ],
       slides: [
-        'First',
-        'Second',
-        'Third',
-        'Fourth',
-        'Fifth'
+        'Découvrez notre jeu monopoly :)',
+        'Convertissez vos achats en crédit',
+        'Entrez vos coupons',
+        'De nombreux cadeaux à gagner !',
+        'Amusez vous bien ;)',
+        'Hello World !'
       ]
     }
   }
@@ -60,10 +95,6 @@ export default {
 <style scoped>
 .menuTitle {
   cursor: pointer;
-}
-
-.v-card {
-  color: whitesmoke !important;
 }
 
 </style>
